25316872f42d97e3a6aecdb788cedb464a875f44
Mateusz-11 <tweak.night@gmail.com>
2023-06-28 21:03:49 +0200

    Bulid the Delete Button

--- client/src/components/ListTodos.js
+++ client/src/components/ListTodos.js
@@ -2,6 +2,22 @@ import React, { useEffect, useState } from "react";
 
 const ListTodos = () => {
 	const [todos, setTodos] = useState([]);
+
+	// delete todo function
+
+	const deleteTodo = async (id) => {
+		try {
+			const deleteTodo = await fetch(`http://localhost:5000/todos/${id}`, {
+				method: "DELETE",
+			});
+			// console.log(deleteTodo);
+
+			setTodos(todos.filter((todo) => todo.todo_id !== id));
+		} catch (err) {
+			console.error(err.message);
+		}
+	};
+
 	const getTodos = async () => {
 		try {
 			const response = await fetch("http://localhost:5000/todos");
@@ -35,10 +51,16 @@ const ListTodos = () => {
 						<td>john@example.com</td>
 					</tr> */}
 					{todos.map((todo) => (
-						<tr>
+						<tr key={todo.todo_id}>
 							<td>{todo.description}</td>
 							<td>Edit</td>
-							<td>Delete</td>
+							<td>
+								<button
+									className='btn btn-danger'
+									onClick={() => deleteTodo(todo.todo_id)}>
+									Delete
+								</button>
+							</td>
 						</tr>
 					))}
 				</tbody>
