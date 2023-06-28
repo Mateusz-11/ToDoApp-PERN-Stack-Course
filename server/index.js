e28c96a2189780de689982dcdf3a1bac64e96f6a
Mateusz-11 <tweak.night@gmail.com>
2023-06-28 12:14:54 +0200

    Update

--- server/index.js
+++ server/index.js
@@ -3,13 +3,27 @@ const app = express();
 const cors = require("cors");
 const pool = require("./db");
 
-// middleware
+//middleware
 app.use(cors());
-app.use(express.json());
+app.use(express.json()); //req.body
 
-// ROUTES //
+//ROUTES//
 
 //create a todo
+
+app.post("/todos", async (req, res) => {
+	try {
+		const { description } = req.body;
+		const newTodo = await pool.query(
+			"INSERT INTO todo (description) VALUES($1) RETURNING *",
+			[description]
+		);
+
+		res.json(newTodo.rows[0]);
+	} catch (err) {
+		console.error(err.message);
+	}
+});
 
 //get all todos
 
